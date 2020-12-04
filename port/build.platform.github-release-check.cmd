5f8d5c077cda46745ecc26b50dc24820fddae968
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:56:16 +0200

    update

--- port/build.platform.github-release-check.cmd
+++ port/build.platform.github-release-check.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release-check quantum-script-extension-datetime
+echo -^> github-release-check quantum-script-extension-datetime
 
 set PROJECT=quantum-script-extension-datetime
 if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
