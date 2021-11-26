0b1705f453fb1cb6b781f2f5d06993828d548b9e
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:21:45 +0200

    Update

--- source/quantum-script-extension-datetime-version.cpp
+++ source/quantum-script-extension-datetime-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace DateTime {
 				namespace Version {
 
-					static const char *version_ = "2.4.0";
+					static const char *version_ = "2.7.0";
 					static const char *build_ = "21";
-					static const char *versionWithBuild_ = "2.4.0.21";
-					static const char *datetime_ = "2021-11-15 16:51:51";
+					static const char *versionWithBuild_ = "2.7.0.21";
+					static const char *datetime_ = "2021-11-22 17:17:34";
 
 					const char *version() {
 						return version_;
